3ff8a7d1fea3833e39e4a43b94984bfe22b3f1f7
kuba-laptop <kuba.jadrzak@gmail.com>
2025-04-14 03:01:07 +0200

    order display of user page rubits based on order of creation for rubits and comments and order of liking for liked rubits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = current_user
 
-    @rubits = current_user.root_rubits
-    @comments = current_user.comments
-    @liked_rubits = current_user.liked_rubits
-    
+    @rubits = current_user.root_rubits.order(created_at: :desc)
+    @comments = current_user.comments.order(created_at: :desc)
+    @liked_rubits = Rubit.joins(:likes)
+                         .where(likes: { user_id: current_user.id })
+                         .order('likes.created_at DESC')
   end
 end
