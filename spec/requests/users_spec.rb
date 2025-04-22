8c376b5e6577d1b4f497951e001ddadaa918375b
kuba-laptop <kuba.jadrzak@gmail.com>
2025-04-22 12:28:26 +0200

    create basic user request tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,15 +1,65 @@
 require "rails_helper"
 
-RSpec.describe "Users", type: :request do
-  describe "GET /user" do
-    let(:user) { create(:user) }
+RSpec.describe "User account creation management", type: :request do
+  it "creates a User with valid attributes and redirects to root page" do
+    get "/users/sign_up"
+    expect(response).to render_template("devise/registrations/new")
 
-    before do
-      sign_in user
-    end
-    it "returns a successful response" do
-      get user_path
-      expect(response).to have_http_status(:ok)
-    end
+    post "/users", params: {
+                     user: {
+                       username: "test",
+                       email: "test@test.test",
+                       password: "password",
+                       password_confirmation: "password",
+                     },
+                   }
+    expect(User.count).to eq(1)
+    expect(response).to redirect_to("/")
+    follow_redirect!
+    expect(response).to render_template("rubits/index")
+  end
+
+  it "does not create a User with invalid attributes and re-renders sign up page" do
+    get "/users/sign_up"
+    expect(response).to render_template("devise/registrations/new")
+
+    post "/users", params: {
+                     user: {
+                       username: "test",
+                       email: "test@test.test",
+                       password: "password1",
+                       password_confirmation: "password2",
+                     },
+                   }
+    expect(User.count).to eq(0)
+    expect(response).to render_template("devise/registrations/new")
+  end
+end
+
+RSpec.describe "User session management", type: :request do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+
+  it "displays user show page when user is logged in" do
+    get "/user"
+    expect(response).to render_template("users/show")
+  end
+
+  it "redirects to sign_in page when user is not logged in" do
+    sign_out user
+    get "/user"
+    expect(response).to redirect_to(new_user_session_path)
+  end
+
+  it "signs out a User and redirect to root page" do
+    get "/user"
+    expect(response).to render_template("users/show")
+
+    delete "/users/sign_out"
+    expect(response).to redirect_to("/")
+    follow_redirect!
+    expect(response).to render_template("rubits/index")
   end
 end
