c6c52c9fb37d4cd78774d07ed9f21df76593c873
kuba-laptop <kuba.jadrzak@gmail.com>
2025-05-13 19:55:15 +0200

    add hash url to account URL path that allows to choose redirect to specific sidebar option

--- app/javascript/controllers/user_sidebar_controller.js
+++ app/javascript/controllers/user_sidebar_controller.js
@@ -1,10 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-  connect() {
-
-  }
   static targets = [
     "rubitsSection",
     "commentsSection",
@@ -12,41 +8,46 @@ export default class extends Controller {
     "orderHistorySection"
   ]
 
+  connect() {
+    const hash = window.location.hash.replace("#", "")
+    if (hash) this.toggleContent(hash)
+    else this.toggleContent("rubits")
+  }
+
   toggleRubitsSection() {
+    this.updateURLHash("rubits")
     this.toggleContent("rubits")
   }
 
   toggleCommentsSection() {
+    this.updateURLHash("comments")
     this.toggleContent("comments")
   }
 
   toggleLikedRubitsSection() {
+    this.updateURLHash("likedRubits")
     this.toggleContent("likedRubits")
   }
+
   toggleOrderHistorySection() {
+    this.updateURLHash("orderHistory")
     this.toggleContent("orderHistory")
   }
 
-
   toggleContent(contentType) {
-    this.hideAllSections();
-
-    if (contentType === "rubits") {
-      this.rubitsSectionTarget.classList.remove("d-none");
-    } else if (contentType === "comments") {
-      this.commentsSectionTarget.classList.remove("d-none");
-    } else if (contentType === "likedRubits") {
-      this.likedRubitsSectionTarget.classList.remove("d-none");
-    } else if (contentType === "orderHistory") {
-      this.orderHistorySectionTarget.classList.remove("d-none");
-    }
+    this.hideAllSections()
+    const section = this[`${contentType}SectionTarget`]
+    if (section) section.classList.remove("d-none")
   }
 
-
   hideAllSections() {
-    this.rubitsSectionTarget.classList.add("d-none");
-    this.commentsSectionTarget.classList.add("d-none");
-    this.likedRubitsSectionTarget.classList.add("d-none");
-    this.orderHistorySectionTarget.classList.add("d-none");
+    this.rubitsSectionTarget.classList.add("d-none")
+    this.commentsSectionTarget.classList.add("d-none")
+    this.likedRubitsSectionTarget.classList.add("d-none")
+    this.orderHistorySectionTarget.classList.add("d-none")
+  }
+
+  updateURLHash(fragment) {
+    history.replaceState(null, null, `#${fragment}`)
   }
 }
